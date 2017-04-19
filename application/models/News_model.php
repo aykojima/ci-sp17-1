c4e6884fd2ab136f07712632ec00fd0a034f7732
Ayumi Kojima <ayumikjm@gmail.com>
2017-04-18 17:33:49 -0700

    Added create class

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -16,5 +16,20 @@ class News_model extends CI_Model {
 
             $query = $this->db->get_where('sp17_news', array('slug' => $slug));
             return $query->row_array();
-    }
-}
+    }//end get_news class
+    
+    public function set_news()
+    {
+        $this->load->helper('url');
+
+        $slug = url_title($this->input->post('title'), 'dash', TRUE);
+
+        $data = array(
+            'title' => $this->input->post('title'),
+            'slug' => $slug,
+            'text' => $this->input->post('text')
+        );
+
+        return $this->db->insert('news', $data);
+    }//edn set_news class
+}//end News_model class
---
+++ application/views/news/success.php
@@ -0,0 +1,5 @@
+<?php //application/views/news/success.php
+?>
+
+
+<p><b>Yay! you successfully created a news item!</b></p>
